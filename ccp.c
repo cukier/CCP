324e3fb1dbd681f09bf5a565b7e35d5559039d39
cuki <mauricio@krebs.com.br>
2014-02-10 15:32:36 -0200

    pwm duty 0 :   0% 	   256 : 100%

--- ccp.c
+++ ccp.c
@@ -4,11 +4,13 @@
 
 #use delay(crystal=15MHz, clock=60MHz)
 
+int duty = 50;
+
 int main(void) {
 
 	setup_ccp1(CCP_PWM);
-	setup_timer_2(T2_DIV_BY_1, 127, 1);
-	set_pwm1_duty(35000);
+	setup_timer_2(T2_DIV_BY_1, 255, 1);
+	set_pwm1_duty(230);
 
 	while (TRUE)
 		;
