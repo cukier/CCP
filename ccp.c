b6af1891068fd827d672056cfb3b89ce8165e7b9
cuki <mauriciocukier@gmail.com>
2014-09-24 16:05:54 -0300

    pwm ok

--- ccp.c
+++ ccp.c
@@ -1,11 +1,20 @@
 #include<18F252.h>
+#DEVICE ADC=10
 
 #FUSES HS
 
 #use delay(clock=15MHz)
-#use rs232(xmit=PIN_C6, baud=9600)
+//#use rs232(xmit=PIN_C6, baud=9600)
+
+#define latencia 200
+
+long cont, read, aux;
 
 int main(void) {
+
+	setup_adc(ADC_CLOCK_INTERNAL);
+	setup_adc_ports(AN0);
+	set_adc_channel(0);
 
 	setup_timer_2(T2_DIV_BY_1, 0xFF, 1);
 
@@ -14,6 +23,13 @@ int main(void) {
 
 	while (TRUE) {
 
+		delay_us(10);
+		read = read_adc();
+		if (aux != read) {
+			aux = read;
+			set_pwm1_duty(read_adc());
+//			printf("pwm: %lu\n\r", read);
+		}
 	}
 
 	return 0;
