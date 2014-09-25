24e51cf74b6f5f97c1ebf3af33589f9710471850
cuki <mauriciocukier@gmail.com>
2014-09-25 17:32:48 -0300

    bkp

--- ccp.c
+++ ccp.c
@@ -8,45 +8,89 @@
 #use delay(clock=15MHz)
 //#use rs232(xmit=PIN_C6, baud=9600)
 
-#define max 24
+#define max			24
+#define sync_pin	PIN_B0
+#define btn_pin		PIN_B1
+#define trig_pin	PIN_C0
+#define tmr0reg		44701
+#define tmr1reg		3285
+
+short sync;
+short btn;
+short ctlrSync;
+short ctrlbtn;
 
 long pwm_cont;
 
 long pwm_duty[max] = { 512, 644, 768, 874, 955, 1006, 1023, 1006, 955, 874, 768,
 		644, 512, 379, 256, 150, 68, 17, 1, 17, 68, 150, 256, 379 };
 
-long get_sen(int index) {
-	double ang = index * 47.1239 / 180;
-	double aux = sin(ang);
-	long ret = (aux + 1) * 512;
-	return ret;
-//	return (long) (sin(index * 47.1239 / 180) + 1) * 512;
-}
-
 #INT_TIMER2
 void isr_timer2() {
 	clear_interrupt(INT_TIMER2);
 	if (pwm_cont >= max)
 		pwm_cont = 0;
-//	long teste = get_sen(pwm_cont);
-//	long teste2 = pwm_duty[pwm_cont];
 	set_pwm1_duty(pwm_duty[pwm_cont++]);
-//	set_pwm1_duty(get_sen(pwm_cont++));
+}
+
+#INT_TIMER1
+void isr_timer1() {
+	clear_interrupt(INT_TIMER1);
+	disable_interrupts(INT_TIMER1);
+	setup_ccp1(CCP_OFF);
+}
+
+#INT_TIMER0
+void isr_timer0() {
+	clear_interrupt(INT_TIMER0);
+	disable_interrupts(INT_TIMER0);
+	set_timer1(tmr1reg);
+	clear_interrupt(INT_TIMER1);
+	enable_interrupts(INT_TIMER1);
+	output_low(trig_pin);
+	delay_us(10);
+	output_high(trig_pin);
 }
 
 int main(void) {
+	output_high(trig_pin);
 
+	setup_timer_0(T0_INTERNAL | T0_DIV_1);
+	setup_timer_1(T1_INTERNAL | T1_DIV_BY_1);
 	setup_timer_2(T2_DIV_BY_16, 0xFF, 16);
 
-	setup_ccp1(CCP_PWM);
+	setup_ccp1(CCP_OFF);
 	set_pwm1_duty(512);
 
 	clear_interrupt(INT_TIMER2);
-	enable_interrupts(INT_TIMER2);
-	enable_interrupts(GLOBAL);
+	clear_interrupt(INT_TIMER1);
+	clear_interrupt(INT_TIMER0);
 
-	while (TRUE)
-		;
+	delay_ms(100);
+
+	while (TRUE) {
+		sync = !input(sync_pin);
+		btn = !input(btn_pin);
+
+		if (btn && !ctrlbtn) {
+			ctrlbtn = TRUE;
+			sync = TRUE;
+		}
+
+		if (sync && !ctlrSync) {
+			ctlrSync = TRUE;
+			ctrlbtn = TRUE;
+			setup_ccp1(CCP_PWM);
+			set_timer0(tmr0reg);
+			clear_interrupt(INT_TIMER2);
+			clear_interrupt(INT_TIMER0);
+			enable_interrupts(INT_TIMER0);
+			enable_interrupts(INT_TIMER2);
+			enable_interrupts(GLOBAL);
+		} else if (!sync && ctlrSync) {
+			ctlrSync = FALSE;
+		}
+	}
 
 	return 0;
 }
