630375dfb7096657095ea8170f36aed00cd75354
cuki <mauriciocukier@gmail.com>
2014-09-25 10:48:47 -0300

    pwm senoidal

--- ccp.c
+++ ccp.c
@@ -6,31 +6,34 @@
 #use delay(clock=15MHz)
 //#use rs232(xmit=PIN_C6, baud=9600)
 
-#define latencia 200
+#define max 24
 
-long cont, read, aux;
+long cont;
+
+long pwm_duty[max] = { 512, 644, 768, 874, 955, 1006, 1023, 1006, 955, 874, 768,
+		644, 512, 379, 256, 150, 68, 17, 1, 17, 68, 150, 256, 379 };
+
+#INT_TIMER2
+void isr_timer2() {
+	clear_interrupt(INT_TIMER2);
+	if (cont >= max)
+		cont = 0;
+	set_pwm1_duty(pwm_duty[cont++]);
+}
 
 int main(void) {
 
-	setup_adc(ADC_CLOCK_INTERNAL);
-	setup_adc_ports(AN0);
-	set_adc_channel(0);
-
-	setup_timer_2(T2_DIV_BY_1, 0xFF, 1);
+	setup_timer_2(T2_DIV_BY_1, 0xFF, 16);
 
 	setup_ccp1(CCP_PWM);
 	set_pwm1_duty(512);
 
-	while (TRUE) {
+	clear_interrupt(INT_TIMER2);
+	enable_interrupts(INT_TIMER2);
+	enable_interrupts(GLOBAL);
 
-		delay_us(10);
-		read = read_adc();
-		if (aux != read) {
-			aux = read;
-			set_pwm1_duty(read_adc());
-//			printf("pwm: %lu\n\r", read);
-		}
-	}
+	while (TRUE)
+		;
 
 	return 0;
 }
