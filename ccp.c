7d3b71effff1306665dec8504e9b13fef3c83410
cuki <mauriciocukier@gmail.com>
2014-10-03 16:00:55 -0300

    3 pwms com 18F45K22

--- ccp.c
+++ ccp.c
@@ -1,26 +1,22 @@
-#include<18F252.h>
+#include<18F45K22.h>
 #DEVICE ADC=10
 
 #include<MATH.H>
 
-#FUSES HS
+#FUSES HSH
 
-#use delay(clock=15MHz)
+#use delay(clock=16MHz)
 //#use rs232(xmit=PIN_C6, baud=9600)
 
 #define max			24
-#define sync_pin	PIN_B0
-#define btn_pin		PIN_B1
-#define trig_pin	PIN_C0
-#define tmr0reg		44701
-#define tmr1reg		3285
+#define btn_pin		PIN_B0
 
-short sync;
 short btn;
-short ctlrSync;
 short ctrlbtn;
 
-long pwm_cont;
+int pwm_cont1 = 0;
+int pwm_cont2 = 8;
+int pwm_cont3 = 16;
 
 long pwm_duty[max] = { 512, 644, 768, 874, 955, 1006, 1023, 1006, 955, 874, 768,
 		644, 512, 379, 256, 150, 68, 17, 1, 17, 68, 150, 256, 379 };
@@ -28,67 +24,69 @@ long pwm_duty[max] = { 512, 644, 768, 874, 955, 1006, 1023, 1006, 955, 874, 768,
 #INT_TIMER2
 void isr_timer2() {
 	clear_interrupt(INT_TIMER2);
-	if (pwm_cont >= max)
-		pwm_cont = 0;
-	set_pwm1_duty(pwm_duty[pwm_cont++]);
+	if (pwm_cont1 >= max)
+		pwm_cont1 = 0;
+	set_pwm1_duty(pwm_duty[pwm_cont1++]);
 }
 
-#INT_TIMER1
-void isr_timer1() {
-	clear_interrupt(INT_TIMER1);
-	disable_interrupts(INT_TIMER1);
-	setup_ccp1(CCP_OFF);
+#INT_TIMER4
+void isr_timer4() {
+	clear_interrupt(INT_TIMER4);
+	if (pwm_cont2 >= max)
+		pwm_cont2 = 0;
+	set_pwm2_duty(pwm_duty[pwm_cont2++]);
 }
 
-#INT_TIMER0
-void isr_timer0() {
-	clear_interrupt(INT_TIMER0);
-	disable_interrupts(INT_TIMER0);
-	set_timer1(tmr1reg);
-	clear_interrupt(INT_TIMER1);
-	enable_interrupts(INT_TIMER1);
-	output_low(trig_pin);
-	delay_us(10);
-	output_high(trig_pin);
+#INT_TIMER6
+void isr_timer6() {
+	clear_interrupt(INT_TIMER6);
+	if (pwm_cont3 >= max)
+		pwm_cont3 = 0;
+	set_pwm3_duty(pwm_duty[pwm_cont3++]);
 }
 
 int main(void) {
-	output_high(trig_pin);
 
-	setup_timer_0(T0_INTERNAL | T0_DIV_1);
-	setup_timer_1(T1_INTERNAL | T1_DIV_BY_1);
 	setup_timer_2(T2_DIV_BY_16, 0xFF, 16);
+	setup_timer_4(T2_DIV_BY_16, 0xFF, 16);
+	setup_timer_6(T2_DIV_BY_16, 0xFF, 16);
 
 	setup_ccp1(CCP_OFF);
+	setup_ccp2(CCP_OFF);
+	setup_ccp3(CCP_OFF);
+
 	set_pwm1_duty(512);
+	set_pwm2_duty(512);
+	set_pwm3_duty(512);
 
 	clear_interrupt(INT_TIMER2);
-	clear_interrupt(INT_TIMER1);
-	clear_interrupt(INT_TIMER0);
+	clear_interrupt(INT_TIMER4);
+	clear_interrupt(INT_TIMER6);
 
 	delay_ms(100);
 
 	while (TRUE) {
-		sync = !input(sync_pin);
+
 		btn = !input(btn_pin);
 
 		if (btn && !ctrlbtn) {
 			ctrlbtn = TRUE;
-			sync = TRUE;
-		}
-
-		if (sync && !ctlrSync) {
-			ctlrSync = TRUE;
-			ctrlbtn = TRUE;
 			setup_ccp1(CCP_PWM);
-			set_timer0(tmr0reg);
+			setup_ccp2(CCP_PWM);
+			setup_ccp3(CCP_PWM);
 			clear_interrupt(INT_TIMER2);
-			clear_interrupt(INT_TIMER0);
-			enable_interrupts(INT_TIMER0);
+			clear_interrupt(INT_TIMER4);
+			clear_interrupt(INT_TIMER6);
 			enable_interrupts(INT_TIMER2);
+			enable_interrupts(INT_TIMER4);
+			enable_interrupts(INT_TIMER6);
 			enable_interrupts(GLOBAL);
-		} else if (!sync && ctlrSync) {
-			ctlrSync = FALSE;
+		} else if (!btn && ctrlbtn) {
+			ctrlbtn = FALSE;
+
+			setup_ccp1(CCP_OFF);
+			setup_ccp2(CCP_OFF);
+			setup_ccp3(CCP_OFF);
 		}
 	}
 
