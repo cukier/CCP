ed881eab053283033cfaddce4b34a7fc695bf6a2
cuki <mauriciocukier@gmail.com>
2014-10-09 17:06:27 -0300

    pwm 18f252 baseado na leitrua analogica

--- ccp.c
+++ ccp.c
@@ -1,59 +1,31 @@
 #include<18F252.h>
 #DEVICE ADC=10
 
-#include<MATH.H>
-
 #FUSES HS
 
-#use delay(clock=16MHz)
-//#use rs232(xmit=PIN_C6, baud=9600)
+#use delay(clock=15MHz)
+#use rs232(xmit=PIN_C6, baud=9600)
 
-#define max			24
-#define btn_pin		PIN_B0
+long leitura;
 
-short btn;
-short ctrlbtn;
-
-int pwm_cont1 = 0;
-
-long pwm_duty[max] = { 512, 644, 768, 874, 955, 1006, 1023, 1006, 955, 874, 768,
-		644, 512, 379, 256, 150, 68, 17, 1, 17, 68, 150, 256, 379 };
-
-#INT_TIMER2
-void isr_timer2() {
-	clear_interrupt(INT_TIMER2);
-	if (pwm_cont1 >= max)
-		pwm_cont1 = 0;
-	set_pwm1_duty(pwm_duty[pwm_cont1++]);
-}
-
+#zero_ram
 int main(void) {
 
-	setup_timer_2(T2_DIV_BY_16, 0xFF, 16);
+	setup_timer_2(T2_DIV_BY_1, 255, 16);
 
-	setup_ccp1(CCP_OFF);
+	setup_ccp1(CCP_PWM);
 
-	set_pwm1_duty(pwm_duty[pwm_cont1++]);
-
-	clear_interrupt(INT_TIMER2);
-
-	delay_ms(100);
+	setup_adc_ports(AN0);
+	setup_adc(ADC_CLOCK_INTERNAL);
+	set_adc_channel(0);
+	delay_us(100);
 
 	while (TRUE) {
-
-		btn = !input(btn_pin);
-
-		if (btn && !ctrlbtn) {
-			ctrlbtn = TRUE;
-			setup_ccp1(CCP_PWM);
-			clear_interrupt(INT_TIMER2);
-			enable_interrupts(INT_TIMER2);
-			enable_interrupts(GLOBAL);
-		} else if (!btn && ctrlbtn) {
-			ctrlbtn = FALSE;
-			setup_ccp1(CCP_OFF);
-		}
+		leitura = read_adc();
+		set_pwm1_duty(leitura);
+		printf("%lu\n\r", leitura);
+		delay_ms(750);
 	}
-
 	return 0;
 }
+
--- makefile
+++ makefile
@@ -1,15 +1,10 @@
 CC=ccsc
 CFLAGS=+FH +LN -T -A -M -Z +DF +Y=9 +STDOUT
 
-all: ccp ccp2 ccp3
-
+all: ccp
 
 ccp: ccp.c
 	$(CC) $(CFLAGS) ccp.c
-ccp2: ccp2.c
-	$(CC) $(CFLAGS) ccp2.c
-ccp3: ccp3.c
-	$(CC) $(CFLAGS) ccp3.c
 
 clean:
 	rm *.cof *.err *.esym *.hex *.lst *.PWI *.bak *.STA *.sym *.tre *.DBK
