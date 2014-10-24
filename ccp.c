1479948b0c5e8d1259a1dc536827a63b8730e90c
cuki <mauriciocukier@gmail.com>
2014-10-24 17:53:29 -0200

    usando 2 pwms com 18F45k22

--- ccp.c
+++ ccp.c
@@ -1,28 +1,44 @@
-#include<18F252.h>
+#include<18F45K22.h>
 #DEVICE ADC=10
 
-#FUSES HS
+#FUSES HSH, PLLEN, CCP2C1
 
-#use delay(clock=15MHz)
+#use delay(clock=60MHz, crystal=15MHz)
+//#use delay(clock=15MHz)
 #use rs232(xmit=PIN_C6, baud=9600)
 
-long leitura;
+long leitura1;
+long leitura2;
+
+long read(int canal) {
+	set_adc_channel(canal);
+	delay_us(100);
+	return read_adc();
+}
 
 #zero_ram
 int main(void) {
 
 	setup_timer_2(T2_DIV_BY_1, 255, 1);
+//	setup_timer_4(T4_DIV_BY_1, 255, 1);
 
 	setup_ccp1(CCP_PWM);
+	setup_ccp2(CCP_PWM);
 
-	setup_adc_ports(AN0);
+	setup_adc_ports(sAN0 | sAN1);
 	setup_adc(ADC_CLOCK_INTERNAL);
 	set_adc_channel(0);
 	delay_us(100);
 
 	while (TRUE) {
-		leitura = read_adc();
-		set_pwm1_duty(leitura);
+		leitura1 = read(0);
+		set_pwm1_duty(leitura1);
+
+		leitura2 = read(1);
+		set_pwm2_duty(leitura2);
+
+		printf("l1 %4lu - l2 %4lu\n\r", leitura1, leitura2);
+		delay_ms(1000);
 	}
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,10 +1,22 @@
 CC=ccsc
-CFLAGS=+FH +LN -T -A -M -Z +DF +Y=9 +STDOUT
+PK2=pk2cmd
 
-all: ccp
+DEVICE=PIC18F25K22
+UNIT1=CCP
+UNIT1_FILE=ccp
 
-ccp: ccp.c
-	$(CC) $(CFLAGS) ccp.c
+HFLAGS=+FH +LN -T -A -M -Z +DF +Y=9 +STDOUT +EA
+BFLAGS=+FB +LN -T -A -M -Z +DF +Y=9 +STDOUT +EA
+MFLAGS=+FM +LN -T -A -M -Z +DF +Y=9 +STDOUT +EA
+PK2FLAGS=-E -P$(DEVICE) -M -R -F
+
+all: $(UNIT1)
+
+$(UNIT1): $(UNIT1_FILE).c
+	$(CC) $(HFLAGS) $(UNIT1_FILE).c
+	
+Burn:
+	$(PK2) $(PK2FLAGS) $(UNIT1_FILE).hex
 
 clean:
-	rm *.cof *.err *.esym *.hex *.lst *.PWI *.bak *.STA *.sym *.tre *.DBK
+	rm *.cof *.err *.esym *.hex *.lst *.pjt *.STA *.sym *.tre *.MCP *.PWI *.DBK
